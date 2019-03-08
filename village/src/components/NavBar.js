32476eda607cc684d1913426e4281eee3aa7ef2b
novinary <novina.dev@gmail.com>
2019-03-08 11:05:08 +0000

    Add margin between the nav links

--- village/src/components/NavBar.js
+++ village/src/components/NavBar.js
@@ -6,7 +6,7 @@ class NavBar extends React.Component {
     render() {
         return (
             <header>
-                <NavLink to="/">Home</NavLink>
+                <NavLink className='mr2' to="/">Home</NavLink>
                 <NavLink to="/smurf-form">Add</NavLink>
             </header>
         )
